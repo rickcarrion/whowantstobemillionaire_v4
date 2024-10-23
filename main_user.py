7782b842de5896ce90569b2b64f44751ac86ae26
rickcarrion <ricardocarrion1506@gmail.com>
2024-10-23 11:04:48 -0500

    a

--- main_user.py
+++ main_user.py
@@ -369,19 +369,17 @@ class UserGUI:
         datetime_question_started = st.session_state.datetime_question_started.strftime('%Y-%m-%d %H:%M:%S')
 
         if (st.session_state.current_session_status == 'playing') & (st.session_state.keep_playing) & (st.session_state.last_page == 'question_page'):
-            now = datetime.now(self.miami_tz)
-            # st.write(now)
-            # st.write(st.session_state.playing_at)
-            # st.write(type(st.session_state.playing_at))
+            placeholder = st.empty()
+            with placeholder.container():
+                now = datetime.now(self.miami_tz)
+                localized_datetime = self.miami_tz.localize(st.session_state.playing_at)
 
-            localized_datetime = self.miami_tz.localize(st.session_state.playing_at)
-
-            seconds_to_start = (localized_datetime - now).total_seconds()
-            if seconds_to_start < 0:
-                pass
-            else:
-                placeholder7 = st.empty()
-                with placeholder7.container():
+                seconds_to_start = (localized_datetime - now).total_seconds()
+                if seconds_to_start < 0:
+                    pass
+                else:
+                    # placeholder7 = st.empty()
+                    # with placeholder7.container():
                     with st.spinner("Waiting for other players..."):
                         now = datetime.now(self.miami_tz)
                         seconds_to_start = (localized_datetime - now).total_seconds()
@@ -389,10 +387,7 @@ class UserGUI:
                         if seconds_to_start > 0:
                             time.sleep(seconds_to_start)
 
-                placeholder7.empty()
-
-            placeholder = st.empty()
-            with placeholder.container():
+                    # placeholder7.empty()
             # with st.empty():
                 if not st.session_state.disable_question_buttons:
                     st.markdown(self.centered_buttons_questions, unsafe_allow_html=True)
